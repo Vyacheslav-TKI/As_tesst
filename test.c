804090a510ffe2c214ca095c92090880bec1aabb
Vyacheslav-TKI <lukankinv20@gmail.com>
2025-06-22 18:56:14 +0300

    test prog for looking vivible headers math.h library in compiler

---
+++ test.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "math.h"
+
+int main()
+{
+    int a = 2, b = 5;
+
+    if (a < b)
+    {
+        return pow(a, b);
+    }
+
+    if (a == b)
+    {
+        return 0;
+    }
+    return 0;
+}
